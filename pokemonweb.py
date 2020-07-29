3482b8ff7bda5e99d42299d784517b47e8c3dbb4
C. Xie <32278569+cxie00@users.noreply.github.com>
2020-07-29 11:03:21 -0400

    Update pokemonweb.py

--- pokemonweb.py
+++ pokemonweb.py
@@ -2,10 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request, Response
 import requests
 import pokebase as pb
 
-from collections import namedtuple
-POKEMON_COLOR_API = "https://pokeapi.co/api/v2/pokemon-color/"
 POKEMON_API = "https://pokeapi.co/api/v2/pokemon/"
-Pokemon = namedtuple('Pokemon', "name habitat growth_rate shape") 
 app = Flask(__name__)
 
 # flask backend 
@@ -58,76 +55,5 @@ def weakness():
         return render_template("weakness.html", content=userPokemon, immunities=immunities, double_damage=double_damage, quad_damage= quad_damage, pic=imglink )
     return redirect(url_for("home"))
 
-def fetch_data_from_url(url):
-    response = requests.get(url)
-    data = {}
-    error = {}
-
-    if response.status_code != 200:
-        error = {"error": "No data"}
-    else:
-        try:
-            data = response.json()
-        except ValueError:
-            error = {"error": "JSON could not be decoded"}
-
-    return data, error
-
-
-def get_pokemon_data(poke_data):
-    for pokemon in poke_data.get("pokemon_species"):
-        name = pokemon.get("name")
-        url = pokemon.get("url")
-        details, error = fetch_data_from_url(url)
-        if error:
-            continue # We are not rendering this pokemon
-        habitat, growth_rate, shape = [
-        details.get(feature).get("name") 
-            if details.get(feature) else "UNKNOWN"
-            for feature in ["habitat", "growth_rate", "shape"]
-        ]
-        yield Pokemon(name=name, habitat=habitat, growth_rate=growth_rate, shape=shape)
-
-# For streaming large data
-# Reference: http://flask.pocoo.org/docs/1.0/patterns/streaming/
-def stream_template(template_name, **context):
-    app.update_template_context(context)
-    t = app.jinja_env.get_template(template_name)
-    rv = t.stream(context)
-    rv.enable_buffering(5)
-    return rv
-@app.route("/pokemon", methods=['GET', 'POST'])
-def pokemon():
-    pokemons = []
-    color = ""
-
-    if request.method == 'POST' and request.form.get('color'):
-        print("pokemon in if")
-        color = request.form.get('color')
-        url = f"{POKEMON_COLOR_API}{color}"
-        data, error = fetch_data_from_url(url)
-        print(data)
-        print(error)
-        if error:
-            return render_template("pokemon.html", error=error, color=color) 
-        else:
-            pokemons = get_pokemon_data(data)
-            return Response(stream_template('pokemon.html', pokemons=pokemons, color=color))
-
-    print("pokemon out of if")
-    return render_template("pokemon.html", pokemons=pokemons, color=color)
-
-
-
 if __name__ == "__main__":
     app.run(debug = True)
-"""
-todo:
-when calculate is clicked, do a get request to search page with search parameter
-can make it submit to 
-look up form submit
-action is the site -- so /pokemon 
-you can send information TO a URL
-    so, to the pokemon page, im gonna give this page: charmander.
-    so i need to figure it out with this route page
-"""
