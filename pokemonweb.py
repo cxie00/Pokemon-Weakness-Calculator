90ee7f163182fa88140fc4ba7236e78c156a30e9
C. Xie <32278569+cxie00@users.noreply.github.com>
2020-08-10 17:03:16 -0400

    Update pokemonweb.py

--- pokemonweb.py
+++ pokemonweb.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 # flask backend 
 
 # render template
+@app.route("/index.html")
+def redir():
+    return redirect(url_for("home"))
+
 @app.route("/")
 def home():
     return render_template("index.html")
