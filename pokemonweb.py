7409734e13aa27f8413e7b7a90eb43e0f23599f7
C. Xie <32278569+cxie00@users.noreply.github.com>
2020-08-10 21:25:46 -0400

    Add files via upload

--- pokemonweb.py
+++ pokemonweb.py
@@ -8,13 +8,9 @@ app = Flask(__name__)
 # flask backend 
 
 # render template
-@app.route("/index.html")
-def redir():
-    return redirect(url_for("home"))
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/about")
 def link():
